1ba2afc42ae67e851065a246e59ef7779ac27afa
Jaeework <jaeework@gmail.com>
2026-02-05 15:17:05 +0900

    feat: integrate API for CRUD operations
    - Implement getTasks, addTask, deleteTask, toggleComplete
    - Update UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -6,24 +7,93 @@ import TodoBoard from "./components/TodoBoard";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import api from "./utils/api";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  const getTasks = async () => {
+    const response = await api.get("/tasks");
+    setTodoList(response.data.data);
+  };
+  const addTask = async () => {
+    if (!todoValue) return;
+    try {
+      const response = await api.post("/tasks", {
+        task: todoValue,
+        isComplete: false,
+      });
+
+      if (response.status === 200) {
+        setTodoValue("");
+        getTasks();
+      } else {
+        throw new Error("task can not be added");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+  const deleteTask = async(id) => {
+    try {
+      const response = await api.delete(`/tasks/${id}`);
+
+      if(response.status === 200) {
+        getTasks();
+      } else {
+        throw new Error("failed to delete the task");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+  const toggleComplete = async(item) => {
+    try {
+      const response = await api.put(`/tasks/${item._id}`, {
+        isComplete: !item.isComplete
+      });
+
+      if(response.status === 200) {
+        getTasks();
+      } else {
+        throw new Error("failed to update the task");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  }
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  }
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
     <Container>
       <Row className="add-item-row">
         <Col xs={12} sm={10}>
           <input
             type="text"
-            placeholder="할일을 입력하세요"
+            placeholder="Add a new task"
             className="input-box"
+            value={todoValue}
+            onChange={(event) => setTodoValue(event.target.value)}
+            onKeyUp={handleKeyDown}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>
+            ADD
+          </button>
         </Col>
       </Row>
 
-      <TodoBoard />
+      <TodoBoard todoList={todoList} deleteTask={deleteTask} toggleComplete={toggleComplete} />
     </Container>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,17 @@
-import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const TodoItem = () => {
+const TodoItem = ({item, deleteTask, toggleComplete}) => {
   return (
     <Row>
       <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+        <div className={`todo-item ${item.isComplete ? "item-complete" : ""}`}>
+          <div className="todo-content">{item.task}</div>
 
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button className="button-delete" onClick={() => deleteTask(item._id)}>Delete</button>
+            <button className="button-delete" onClick={() => toggleComplete(item)}>
+              {item.isComplete ? "Undo" : "Done"}
+            </button>
           </div>
         </div>
       </Col>
